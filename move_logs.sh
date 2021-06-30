5b6631d407855f143089689a061dc6608f65e2a8
Michiel Meeuwissen <git@meeuw.org>
2021-06-30 22:55:25 +0200

    fixed typo

--- move_logs.sh
+++ move_logs.sh
@@ -7,5 +7,5 @@ for i in "${!target_folders[@]}"; do
   OLD="$folder/OLD"
   mkdir -p "$OLD"
   find "$folder" -type f -maxdepth 1 -mtime +3 -not -name "*.gz" -exec gzip {} \; -exec mv {}.gz "OLD/" \;
-  find "$OLD" -type f -name "*.gz" -mtype +14 -exec echo "removing " {} \; -exec gzip {} \;
+  find "$OLD" -type f -name "*.gz" -mtime +30 -exec echo "removing " {} \; -exec gzip {} \;
 done
