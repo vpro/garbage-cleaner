0fb688c2b5982c7e081f7003f2203719111a838a
Michiel Meeuwissen <m.meeuwissen@vpro.nl>
2021-07-09 13:50:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-echo "$CRON /root/purge.sh $TARGET_FOLDERS
-#" > scheduler.txt
-
-echo "@daily /root/move_logs.sh $LOG_FOLDERS
-#" >> scheduler.txt
+echo "$CRON /root/purge.sh $TARGET_FOLDERS" > scheduler.txt
+echo "@daily /root/move_logs.sh $LOG_FOLDERS" >> scheduler.txt
 
 supercronic scheduler.txt
