4ed32fd341fc60c5c9b87b4edda693b534577b98
Michiel Meeuwissen <git@meeuw.org>
2022-06-22 21:27:36 +0200

    Missing -exec

--- move_logs.sh
+++ move_logs.sh
@@ -9,5 +9,5 @@ for i in "${!target_folders[@]}"; do
   echo "Removing old files in $OLD"
   find "$OLD" -maxdepth 1  -type f  -name "*.gz" -mtime +30 -exec echo "removing " {} \; -delete
   echo "Zipping files in $folder and moving them to $OLD"
-  find "$folder" -maxdepth 1 -type f  -mtime +1 -not -name '*.gz' -exec echo zipping {} \; gzip {} \; -exec touch {}.gz \; -exec mv {}.gz "$OLD/" \;
+  find "$folder" -maxdepth 1 -type f  -mtime +1 -not -name '*.gz' -exec echo zipping {} \; -exec gzip {} \; -exec touch {}.gz \; -exec mv {}.gz "$OLD/" \;
 done
