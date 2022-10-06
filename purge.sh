066684fddf7c735996199412002ff111d82d20d9
Michiel Meeuwissen <git@meeuw.org>
2022-10-06 14:51:05 +0200

    Details about atime of directory.

--- purge.sh
+++ purge.sh
@@ -9,6 +9,8 @@ for i in "${!target_folders[@]}"; do
   printf "Recursive deleting files in: ""%s"" older then %s\n" $folder $fileage
   # mindepth 1: don't include the base directories as things to delete
   find "$folder" -type f $mark +$fileage -delete
-  find "$folder" -mindepth 1 -type d $mark +$fileage -empty -delete
+
+  # for directories: always mtime, e.g. placing a file in a directory will _not_ change it's atime.
+  find "$folder" -mindepth 1 -type d -mtime +$fileage -empty -delete
   echo "Done"
 done
