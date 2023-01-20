da54a00e964b78b8abaa9815fec82da3bbe8fde9
Michiel Meeuwissen <git@meeuw.org>
2023-01-20 20:52:42 +0100

    I think this obviously should be mindepth, it doesn't work now.

--- purge.sh
+++ purge.sh
@@ -25,11 +25,11 @@ for i in "${!target_folders[@]}"; do
   fi
   # mindepth 1: don't include the base directories as things to delete
   echo "Removing files in $folder with command" "${fileage[@]}"
-  find "$folder" -maxdepth 1 -type f "${fileage[@]}" -exec echo "removing " {} \; ${action}
+  find "$folder" -mindepth 1 -type f "${fileage[@]}" -exec echo "removing " {} \; ${action}
 
   # for directories: always mtime, e.g. placing a file in a directory will _not_ change it's atime.
   echo "Removing directories in $folder with command -mtime ${fileage[1]}"
-  find "$folder" -maxdepth 1 -type d -mtime "${fileage[1]}" -empty -exec echo "removing " {} \; ${action}
+  find "$folder" -mindepth 1 -type d -mtime "${fileage[1]}" -empty -exec echo "removing " {} \; ${action}
 
   echo "Done"
 done
